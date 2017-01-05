c5eb2a02596c9facc5e498ece82fe345fbc10934
Sujeong Lee <xair0811@gmail.com>
2017-01-05 17:05:19 +0900

    init push with initial activity and configuration for firebase

---
+++ app/src/main/java/io/sujeong/drinkwater/DrinkApplication.java
@@ -0,0 +1,12 @@
+package io.sujeong.drinkwater;
+
+import android.app.Application;
+
+
+public class DrinkApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
