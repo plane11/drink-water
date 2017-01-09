fba739a87c650a92cb1d16bd1ab26c5a30de3b02
Sujeong Lee <xair0811@gmail.com>
2017-01-09 13:12:39 +0900

    add activity move from Splash to DailyStatus Activity

--- app/src/main/java/io/sujeong/drinkwater/activity/SplashActivity.java
+++ app/src/main/java/io/sujeong/drinkwater/activity/SplashActivity.java
@@ -2,7 +2,10 @@ package io.sujeong.drinkwater.activity;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.view.WindowManager;
 
@@ -10,12 +13,27 @@ import io.sujeong.drinkwater.R;
 
 public class SplashActivity extends Activity {
 
-    private final int SPLASH_LOAD_PERIOD = 3000;
+    private final int SPLASH_LOAD_PERIOD = 2000;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                Intent intent = new Intent(SplashActivity.this, DailyStatusActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        handler.sendEmptyMessageDelayed(0, SPLASH_LOAD_PERIOD);
+
     }
 }
