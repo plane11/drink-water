06b64fc100800a368875301ebfab6d3eef16fb64
Sujeong Lee <xair0811@gmail.com>
2017-01-26 18:55:22 +0900

    set custom layout in daily status activity's actionbar

--- app/src/main/java/io/sujeong/drinkwater/activity/DailyStatusActivity.java
+++ app/src/main/java/io/sujeong/drinkwater/activity/DailyStatusActivity.java
@@ -1,8 +1,10 @@
 package io.sujeong.drinkwater.activity;
 
-import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -27,14 +29,16 @@ public class DailyStatusActivity extends AppCompatActivity {
         initActionBar();
 
         loadData();
-
     }
 
     private void initActionBar() {
-        ActionBar actionBar = getActionBar();
+        ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowCustomEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(false);
         actionBar.setDisplayShowTitleEnabled(false);
+
+        View actionBarView = LayoutInflater.from(this).inflate(R.layout.actionbar_drink_status, null);
+        actionBar.setCustomView(actionBarView);
     }
 
     @Override
